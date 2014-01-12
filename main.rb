85189dd8457a85a7a80330828006a103d5592616
Roman Parashchenko <roman.paraschenko@clickworker.com>
2014-01-12 17:48:52 +0200

    Improved text output

--- main.rb
+++ main.rb
@@ -2,15 +2,21 @@ require 'yaml'
 require 'timeout'
 #require 'debugger'
 
-trap("INT") do
-  puts `clear`
+def finish_lesson(text=nil)
+  print `clear`
+  puts text if text
   puts "Спасибо за урок!"
   exit
+end
+
+trap("INT") do
+  finish_lesson
 end
 
 $index = YAML.load_file('data/index.yml')
 
 def parse_exercise_type
+  print `clear`
   puts "Выберите тип урока:"
   puts "\t0. обучение"
   puts "\t1. повторение"
@@ -29,48 +35,49 @@ def sleep_time(type)
   end
 end
 
-def wait_for_user_answer
-  Timeout.timeout(2) do
+def print_message(msg)
+  print `clear`
+  print msg
+end
+
+def wait_for_user_answer(timeout)
+  Timeout.timeout(timeout) do
     return gets
   end
 rescue Timeout::Error
   nil
 end
 
-puts "Выберете категорию:"
+# --- MAIN ---
+
+print_message "Выберете категорию:\n"
 $index.keys.each_with_index{|name, i| puts "\t#{i}. #{name}" }
 
 category_index = gets.to_i rescue 0
 category_name = $index.keys[category_index]
 category_data = YAML.load_file($index[category_name])
 
-puts `clear`
-puts "Выбранная категория '#{category_name}'. Поехали!"
+print_message "Выбранная категория '#{category_name}'. Поехали!\n"
 
 exercise_type = parse_exercise_type
 sleep_time_by_type = sleep_time(exercise_type)
 
 loop do
-  if category_data.empty?
-    puts "Нету слов для изучения"
-    exit 1
-  end
+  finish_lesson "Нету слов для изучения" if category_data.empty?
   category_data.dup.shuffle.each do |word_data|
-    puts `clear`
-    print "\r"
-    print " " * 100
-    print "\r#{word_data["t"]}"
+    sleep 0.5
+    print_message "#{word_data["t"]}"
+    sleep 2
+    print_message "#{word_data["t"]} - #{word_data["p"]} #{word_data["w"]}"
     if exercise_type == :exam
-      sleep 2
+      sleep sleep_time_by_type
     else
-      answer = wait_for_user_answer
+      answer = wait_for_user_answer(sleep_time_by_type)
       if answer == "\n"
         category_data.delete(word_data)
-        next
+        sleep 0.5
       end
     end
-    print "\r#{word_data["t"]} - #{word_data["p"]} #{word_data["w"]}"
-    sleep sleep_time_by_type
     $stdout.flush
   end
 end
