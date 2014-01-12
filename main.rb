64161bf20d63f4211041c85b30f5b35fff578e1b
Roman Parashchenko <roman.paraschenko@clickworker.com>
2014-01-12 12:23:17 +0200

    Handled interrapting, added simple solution to exclude learnt words

--- main.rb
+++ main.rb
@@ -1,5 +1,13 @@
 require 'yaml'
-require 'debugger'
+require 'timeout'
+#require 'debugger'
+
+trap("INT") do
+  puts `clear`
+  puts "Спасибо за урок!"
+  exit
+end
+
 $index = YAML.load_file('data/index.yml')
 
 def parse_exercise_type
@@ -21,6 +29,14 @@ def sleep_time(type)
   end
 end
 
+def wait_for_user_answer
+  Timeout.timeout(2) do
+    return gets
+  end
+rescue Timeout::Error
+  nil
+end
+
 puts "Выберете категорию:"
 $index.keys.each_with_index{|name, i| puts "\t#{i}. #{name}" }
 
@@ -28,16 +44,31 @@ category_index = gets.to_i rescue 0
 category_name = $index.keys[category_index]
 category_data = YAML.load_file($index[category_name])
 
+puts `clear`
 puts "Выбранная категория '#{category_name}'. Поехали!"
 
-sleep_time_by_type = sleep_time(parse_exercise_type)
+exercise_type = parse_exercise_type
+sleep_time_by_type = sleep_time(exercise_type)
 
 loop do
-  category_data.shuffle.each do |word_data|
+  if category_data.empty?
+    puts "Нету слов для изучения"
+    exit 1
+  end
+  category_data.dup.shuffle.each do |word_data|
+    puts `clear`
     print "\r"
     print " " * 100
     print "\r#{word_data["t"]}"
-    sleep 2
+    if exercise_type == :exam
+      sleep 2
+    else
+      answer = wait_for_user_answer
+      if answer == "\n"
+        category_data.delete(word_data)
+        next
+      end
+    end
     print "\r#{word_data["t"]} - #{word_data["p"]} #{word_data["w"]}"
     sleep sleep_time_by_type
     $stdout.flush
